254fa2bc0232231597952afb8dd971e9ad5f2fbf
Jenna_Dunford <Jenna.Dunford@bbd.co.za>
2023-07-18 10:51:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
  
-app.listen(80, function () {
+app.listen(1024, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
