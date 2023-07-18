a6f105235b1b98f55b44fbaeddca964798e81836
Jenna_Dunford <Jenna.Dunford@bbd.co.za>
2023-07-18 14:11:45 +0200

    Update socket.js

--- Utils/socket.js
+++ Utils/socket.js
@@ -56,8 +56,8 @@ function socket(io) {
             usersTemp.forEach((user, index) => {
                 if(user[socket.id]){
                     io.to(roomname).emit('user-left', {username: user.username});
-                    io.to(roomname).emit('user-left', {username: user});
-                    io.to(roomname).emit('user-left', {username: users[roomname][index].username});
+                    io.to(roomname).emit('user-left', {username: usersTemp[index].username});
+                    io.to(roomname).emit('user-left', {username: usersTemp[roomname][index].username});
                     console.log("removinggg");
                     usersTemp.splice(index, 1)
                 }
